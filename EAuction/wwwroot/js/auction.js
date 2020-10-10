274b27963009025f1987d294730637652cd524f3
Siustis Teodor <siustis.teodor@gmail.com>
2020-10-10 19:39:26 +0300

    Fixed logo not showing up for mobile and removed alert from auction.js.

--- EAuction/wwwroot/js/auction.js
+++ EAuction/wwwroot/js/auction.js
@@ -8,15 +8,11 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/auctionhub").build
     connection.on("ReceiveOrderUpdate", function() {
         //const statusDiv = document.getElementById("status");
         //statusDiv.innerHTML = update;
-        alert('received!');
+       // alert('received!');
     }
     );
 
-    connection.on("NewOrder", function (order) {
-        var statusDiv = document.getElementById("status");
-        statusDiv.innerHTML = "Someone ordered an " + order.product;
-    }
-    );
+  
 
     connection.on("finished", function () {
         connection.stop();
